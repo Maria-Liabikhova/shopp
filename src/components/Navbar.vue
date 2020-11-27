c49f14bb7e8d9a28f14c05e9053b1918b71c1a2f
unknown <mashagi@yandex.ru>
2020-11-27 15:41:57 +0300

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-navigation-drawer app  temporary><h1>HELLO</h1></v-navigation-drawer>
+    <v-app-bar app>
+      <v-toolbar dense color="green">
+        <v-app-bar-nav-icon color="#9c0202"></v-app-bar-nav-icon>
+        <v-toolbar-title class="white--text">Online Store</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon color="#9c0202">mdi-magnify</v-icon>
+        </v-btn>
+        <v-btn icon color="#9c0202">
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon color="#9c0202">mdi-dots-vertical</v-icon>
+        </v-btn>
+      </v-toolbar>
+    </v-app-bar>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
