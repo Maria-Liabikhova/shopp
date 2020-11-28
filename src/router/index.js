f8ff3a9e62fb63e6df67eef4f279d78a37dd5193
unknown <mashagi@yandex.ru>
2020-11-28 22:13:25 +0300

    add: new pages and routers

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,43 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
 const routes = [
-  
+  {
+    path:'',
+    component: () => import('../components/Home.vue')
+  },
+  {
+    path:'/login',
+    component: () => import('../components/Auth/Login.vue')
+  },
+  {
+    path: '/registern',
+    component: () => import('../components/Auth/Register.vue')
+  },
+  {
+    path: '/new',
+    component: () => import('../components/Products/NewProduct.vue')
+  },
+  {
+    path:('/product/:id'),
+    component:() => import('../components/Products/Product.vue')
+  },
+  {
+    path:('/list'),
+    component:() => import('../components/Products/ProductList.vue')
+  },
+  {
+    path:('/checkout'),
+    component:() => import('../components/User/Checkout.vue')
+  }
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes
-});
+})
 
 export default router;
