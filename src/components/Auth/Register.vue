95dd9dae5b17b56d3ddd2aea34c03e603815c8e4
unknown <mashagi@yandex.ru>
2020-12-01 13:03:08 +0300

    add: registration page

--- src/components/Auth/Register.vue
+++ src/components/Auth/Register.vue
@@ -1,14 +1,92 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col>
-        <h1>Register</h1>
-      </v-col>
-    </v-row>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md6>
+        <v-card class="elevation-24">
+          <v-toolbar dark color="#9c0202">
+            <v-toolbar-title>Registration form</v-toolbar-title>
+            <!-- <v-spacer></v-spacer> -->
+          </v-toolbar>
+          <v-card-text>
+            <v-form ref="form" v-model="valid">
+              <v-text-field 
+                prepend-icon="mdi-email" 
+                name="email" 
+                label="Email" 
+                type="email"
+                v-model="email"
+                :rules="emailRules"
+                required
+                >
+              </v-text-field>
+              <v-text-field 
+                prepend-icon="mdi-lock" 
+                name="password" 
+                label="Password" 
+                type="password"
+                :rules="passwordRules"
+                :counter="6"
+                v-model="password"
+                required
+                >
+                </v-text-field>
+              <v-text-field 
+                prepend-icon="mdi-repeat" 
+                name="confirm-password" 
+                label="Confirm password" 
+                type="password"
+                :rules="confirmPasswordRules"
+                :counter="6"
+                v-model="confirmPassword"
+                required
+                >
+                </v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn dark  color="#9c0202"
+            @click="onSubmit"
+            :disabled="!valid"
+            >Create account</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      valid: false,
+      email: '',
+      password: '',
+      confirmPassword: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid',
+      ],
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v => v.length >= 6 || 'Password must be equal or than  than 6 characters',
+      ],
+      confirmPasswordRules: [
+        v =>!!v || 'Password is required',
+        v => v === this.password || 'Password should match'
+      ]
+    }
+  },
+  methods: {
+    onSubmit () {
+      if (this.$refs.form.validate()) {
+        const user ={
+          email: this.email,
+          password: this.password
+        }
+        console.log( user)
+      }
+    }
+  }
 }
 </script>
