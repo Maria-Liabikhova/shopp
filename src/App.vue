e74ab25c6d9acafce1ef09761c3f08f03fdbfd7f
unknown <mashagi@yandex.ru>
2021-01-08 20:42:12 +0300

    add: 4 classes depends on screen size

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,14 @@ export default {
       this.$store.dispatch('clearError')
     },
     onResize() {
-      this.$store.dispatch('setMobile', this.isMobile= window.innerWidth > 991 ? "desktop" : "mobile");
+      function isMob() {
+        let screen = window.innerWidth;
+        if ( screen <= 319 ) return "mobile";
+        else if ( screen > 319 && screen <= 767) return "small";
+        else if ( screen > 767 && screen <= 991) return "middle";
+        else return "large";
+      }
+      this.$store.dispatch('setMobile', this.isMobile= isMob())
     },
   },
   computed: {
@@ -50,11 +57,23 @@ export default {
     border: 1px solid rgb(162, 141, 70);
     background-color: green !important;
   }
-  .desktop {
+  .small {
     background-color: salmon !important;
     border: 2px solid yellowgreen;
     border-radius: 10px;
     font-family: fantasy;
     color: indigo 
+  }
+  .middle {
+    background-color: rgb(144, 27, 173) !important;
+    border: 2px solid rgb(199, 22, 60);
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    color: rgb(108, 215, 242) 
+  }
+  .large {
+    background-color: rgb(243, 189, 183) !important;
+    border-bottom: 3px dashed rgb(104, 50, 205);
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    color: aliceblue ;
   }
 </style>
