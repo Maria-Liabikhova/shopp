cf5e82b3e6c3d9f32ff36c9ad3098779553ecb57
unknown <mashagi@yandex.ru>
2021-01-14 21:44:08 +0300

    add: filter by role

--- src/components/User/Users.vue
+++ src/components/User/Users.vue
@@ -14,21 +14,31 @@
               width="100%"
               permanent
             >
-              <!-- <v-list>
-                <v-list-item
-                  v-for="([icon, text], i) in items"
-                  :key="i"
-                  link
-                >
-                  <v-list-item-icon>
-                    <v-icon>{{ icon }}</v-icon>
-                  </v-list-item-icon>
-
-                  <v-list-item-content>
-                    <v-list-item-title>{{ text }}</v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
-              </v-list> -->
+              <div class="text-center menu">
+                <v-menu offset-y open-on-hover>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                      color="primary"
+                      dark
+                      v-bind="attrs"
+                      v-on="on"
+                      
+                    >
+                      Choose users
+                    </v-btn>
+                  </template>
+                  <v-list >
+                    <v-list-item
+                      class="dropdown"
+                      v-for="(item, index) in items"
+                      :key="index"
+                      @click="filter = item.title"
+                    >
+                      <v-list-item-title >{{ item.title }}</v-list-item-title>
+                    </v-list-item>
+                  </v-list>
+                </v-menu>
+              </div>
             </v-navigation-drawer>
           </v-card>
         </v-col>
@@ -36,8 +46,9 @@
         >
           <v-row>
             <v-col  cols="6" xs='6' sm='6' md='4'
-              v-for="crmUser in crmUsers"
-              :key="crmUser.id">
+              v-for="crmUser in filteredList"
+              :key="crmUser.id"
+              >
               <v-card
                 class="mx-auto"
                 max-width="344"
@@ -69,10 +80,13 @@
     data() { 
       return {
         items: [
-          ['mdi-email', 'Inbox'],
-          ['mdi-account-supervisor-circle', 'Supervisors'],
-          ['mdi-clock-start', 'Clock-in'],
+          { title: 'All users' },
+          { title: 'Admin' },
+          { title: 'Editor' },
+          { title: 'Client' },
+          { title: 'Guest' },
         ],
+        filter: "All users"
       }
     },
     computed: {
@@ -81,12 +95,26 @@
       },
       crmUsers() {
         return this.$store.getters.crmUsers
+      },
+      filteredList () {
+        if (this.filter === 'All users') {return this.crmUsers}
+        if (this.filter === 'Admin') {return this.crmUsers.filter(t => t.role === 'Admin')}
+        if (this.filter === 'Editor') {return this.crmUsers.filter(t => t.role === 'Editor')}
+        if (this.filter === 'Client') {return this.crmUsers.filter(t => t.role === 'Client')}
+        return this.crmUsers.filter(t => t.role === 'Guest')
       }
-    }
+    },
   }
 </script>
 
 <style scoped>
+  .dropdown{
+    background-color: coral;
+  }
+  .dropdown:hover{
+    background-color: darksalmon;
+    cursor: pointer;
+  }
   .card__name {
     color: black;
     font-weight: bold;
@@ -102,5 +130,11 @@
   .card__describe {
     color: black;
     font-size: 16px;
+  }
+  .v-list {
+    padding: 0;
+  }
+  .menu {
+    padding-top: 50px;
   }
 </style>
